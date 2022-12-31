cc024d7631f958ad404eac9095fdced70dc92e40
yud0uhu <cist.b219.y.osaka@gmail.com>
2022-12-31 21:44:30 +0900

    fix: ヘッダーの文言を修正

--- src/components/common/header/index.tsx
+++ src/components/common/header/index.tsx
@@ -1,19 +1,15 @@
 import { FC } from 'react'
-import { userFirebaseAuthContext } from '@/lib/firebase/utils/auth'
 
 type HeaderProps = {
   title: string
 }
 const Header: FC<HeaderProps> = ({ title }: HeaderProps) => {
-  const auth = userFirebaseAuthContext()
   return (
     <header>
       <nav className='relative flex w-full items-center justify-between bg-sky-200 py-2 shadow-md'>
         <div className='mx-auto flex max-w-sm items-center space-x-4 p-6'>
           <div>
-            <div className='text-xl font-bold text-white'>
-              {title}@{auth.currentUser ? 'ログイン中' : 'ログインしてね'}
-            </div>
+            <div className='text-xl font-bold text-white'>{title}</div>
           </div>
         </div>
       </nav>
